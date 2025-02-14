74542901d409104e409c6557227380f047d9171e
Rizky Kurniawan <rizky1842@gmail.com>
2025-02-14 14:38:27 +0700

    feat: redirect page

--- src/app/contact-us/page.js
+++ src/app/contact-us/page.js
@@ -6,6 +6,7 @@ import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api"
 import Link from "next/link"
 import { useRouter } from "next/navigation"
 import { useTranslations } from "next-intl"
+import Footer from "@/components/Footer"
 
 const libraries = ["places"]
 const mapContainerStyle = {
@@ -39,17 +40,12 @@ function ContactUs() {
         <div>
             <Navbar visibleOn="desktop" />
             <div className="flex items-center border-[#F0F3F7] px-4 py-3 lg:hidden">
-                <ArrowLeftIcon
-                    className="h-6 w-6"
-                    onClick={() => router.back()}
-                />
+                <ArrowLeftIcon className="h-6 w-6" onClick={() => router.back()} />
                 <div className="ml-2 font-semibold">{t("page.contactus")}</div>
             </div>
             <div className="min-h-screen">
                 <div className="mx-auto hidden max-w-7xl lg:block">
-                    <div className="px-7 pb-1 pt-12 text-2xl font-bold">
-                        {t("page.contactus")}
-                    </div>
+                    <div className="px-7 pb-1 pt-12 text-2xl font-bold">{t("page.contactus")}</div>
                     <div className="px-7 text-[#007185]">
                         <Link href="/profile" className="cursor-pointer">
                             Home
@@ -57,47 +53,26 @@ function ContactUs() {
                         / {t("page.contactus")}
                     </div>
                 </div>
-                <div className="border-b border-[#F0F3F7] lg:mt-6"> </div>
+                <div className="border-b border-[#F0F3F7] lg:mt-6"></div>
                 <div className="mx-auto max-w-7xl lg:p-7">
                     <div className="flex flex-col lg:flex-row">
                         <div className="lg:w-2/5">
                             <div className="bg-white p-4 lg:rounded-xl lg:p-12 lg:shadow">
-                                <div className="text-sm font-bold lg:text-2xl">
-                                    {t("page.helpCenter")}
-                                </div>
-                                <div className="mt-4 text-sm font-normal leading-6">
-                                    {t("page.contactDescription")}
-                                </div>
-                                <div className="mt-10 text-xs font-extrabold text-[#6D7588] lg:text-sm">
-                                    Jakarta
-                                </div>
+                                <div className="text-sm font-bold lg:text-2xl">{t("page.helpCenter")}</div>
+                                <div className="mt-4 text-sm font-normal leading-6">{t("page.contactDescription")}</div>
+                                <div className="mt-10 text-xs font-extrabold text-[#6D7588] lg:text-sm">Jakarta</div>
                                 <div className="mt-4 text-sm font-normal leading-6 opacity-80">
-                                    Jam operasional: <br /> Senin-Jumat |
-                                    9.00-18.00 <br />
-                                    <div
-                                        className="mt-2 w-full cursor-pointer text-sm leading-6 text-[#007185]"
-                                        onClick={() =>
-                                            window.open(
-                                                "https://wa.me/62811833164",
-                                                "_blank",
-                                            )
-                                        }>
+                                    Jam operasional: <br /> Senin-Jumat | 9.00-18.00 <br />
+                                    <div className="mt-2 w-full cursor-pointer text-sm leading-6 text-[#007185]" onClick={() => window.open("https://wa.me/62811833164", "_blank")}>
                                         {t("page.contactMeByWatsapp")}
                                     </div>
-                                    <div className="mt-2 w-full text-sm leading-6 text-[#007185]">
-                                        admin@bulky.id
-                                    </div>
+                                    <div className="mt-2 w-full text-sm leading-6 text-[#007185]">admin@bulky.id</div>
                                 </div>
                             </div>
                         </div>
-                        <div
-                            className="overflow-hidden lg:ml-8 lg:w-3/5 lg:rounded-xl"
-                            style={{ height: "500px" }}>
+                        <div className="overflow-hidden lg:ml-8 lg:w-3/5 lg:rounded-xl" style={{ height: "500px" }}>
                             {isLoaded ? (
-                                <GoogleMap
-                                    mapContainerStyle={mapContainerStyle}
-                                    center={coords}
-                                    zoom={19}>
+                                <GoogleMap mapContainerStyle={mapContainerStyle} center={coords} zoom={19}>
                                     <Marker position={coords} />
                                 </GoogleMap>
                             ) : (
@@ -107,7 +82,7 @@ function ContactUs() {
                     </div>
                 </div>
             </div>
-            {/* <Footer /> */}
+            <Footer />
             <FloatingIcon />
         </div>
     )
---
+++ src/app/redirect/page.js
@@ -0,0 +1,42 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { MoonLoader } from "react-spinners"
+import { useSearchParams } from "next/navigation"
+
+function Redirect() {
+    const orderId = useSearchParams().get("orderId")
+    const [isMobile, setIsMobile] = useState(false)
+
+    useEffect(() => {
+        const checkDevice = () => {
+            setIsMobile(window.innerWidth <= 768) // Anggap <=768px adalah mobile
+        }
+
+        checkDevice() // Cek saat pertama kali render
+        window.addEventListener("resize", checkDevice) // Update saat resize
+
+        return () => window.removeEventListener("resize", checkDevice)
+    }, [])
+
+    useEffect(() => {
+        if (isMobile) {
+            setTimeout(() => {
+                window.location.href = "/"
+            }, 3000)
+        } else {
+            setTimeout(() => {
+                window.location.href = "/order/" + orderId
+            }, 3000)
+        }
+    }, [isMobile])
+
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-white">
+            <div className="mr-2 text-lg text-black">{isMobile ? "Please wait... (Mobile)" : "Please wait... (Desktop)"}</div>
+            <MoonLoader color="#FFCF02" size={30} aria-label="Loading Spinner" />
+        </div>
+    )
+}
+
+export default Redirect
