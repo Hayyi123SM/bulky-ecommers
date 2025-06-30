11a68a7844cb17020d85ff1e3c8d07cf696de1a6
MacBook Pro <macbookpro@192.168.1.12>
2025-06-30 17:22:46 +0700

    add

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,21 +29,32 @@ export default function RootLayout({ children }) {
     return (
         <html lang={locale}>
             <head>
-                <Script
-                    id="google-tag-manager"
-                    strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                            })(window,document,'script','dataLayer','GTM-5S7MDMBK');`,
-                    }}
+            <Script
+                id="gtm-head"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-5S7MDMBK');
+                    `,
+                }}
                 />
                 <link rel="icon" href="/new/favicon bulky-01.png" sizes="any" />
                 <title>Bulky</title>
             </head>
             <body>
+            <noscript>
+                <iframe
+                    src="https://www.googletagmanager.com/ns.html?id=GTM-5S7MDMBK"
+                    height="0"
+                    width="0"
+                    style={{ display: 'none', visibility: 'hidden' }}
+                    title="Google Tag Manager"
+                />
+                </noscript>
                 <Suspense fallback={<div>Loading...</div>}>
                     <NextIntlClientProvider locale={locale} messages={messages}>
                         <ClientProvider>{children}</ClientProvider>
